390f088d3b9a77d0434dafa3a9bf4f603f055316
Eric Grange <egrange@creative-it.net>
2020-12-18 11:00:23 +0100

    Normalize arguments format & doc

--- uCEFSnapshotParameters.pas
+++ uCEFSnapshotParameters.pas
@@ -48,38 +48,38 @@ uses LibTurboJPEG, Vcl.Imaging.pngimage, System.StrUtils;
 const
    cHelp = 'cefHtmlSnaphot utility v0.1 - Html to image or pdf coversion using Chromium Embedded Framework'#10
          + 'Using CEF 87.1.12.0, CEF4Delphi, TurboJPEG see https://github.com/EricGrange/cefHtmlSnapshot'#10#10
-         + 'cefHtmlSnapshot.exe [-arg1 value1] [-arg2 value2] ...'#10#10
-         + '  -?, -h, -help    This inline documentation'#10
-         + '  -url             URL of the website or file to be snapshotted (required)'#10
-         + '  -delay, -d       Delay in milliseconds, between 100 ms and 30 sec (default 1 sec)'#10
-         + '  -out             Output file pathname, extension determines format (default snapshot.bmp)'#10
-         + '                   Supported formats: JPEG, PNG, BMP and PDF'#10
+         + 'cefHtmlSnapshot.exe url_or_file [-arg1 value1] [-arg2 value2] ... output_file'#10
          + #10
-         + '  -width, -w       Width of the snapshot, between 1 and 2048 (default 1024)'#10
-         + '  -height, -h      Height of the snapshot, between 1 and 2048 (default 768)'#10
-         + '                   When output format is a PDF, this parameter is ignored'#10
-         + '  -scale, -s       Scale of the website relative to 96dpi, between 0.1 and 10.0 (default 1.0)'#10
-         + '  -quality         Output JPEG quality (1 to 100, default 90)'#10
-         + '  -compression     Output PNG compresson level (0 to 9, default 7)'#10
+         + '  -?, -h, --help    This inline documentation'#10
+         + '  url_or_file       URL of the website or file to be snapshotted (required)'#10
+         + '  output_file       Output file pathname, extension determines format (default snapshot.bmp)'#10
          + #10
-         + '  -cookie          set a cookie, format is CommaText, starting with name=value, then fields'#10
-         + '                   url=       cookie url (uses scheme + domain from url parameter by default)'#10
-         + '                   domain=    cookie domain (uses domain from url parameter by default)'#10
-         + '                   path=      cookie path ("/" by default)'#10
-         + '                   secure=    secure flag (0 or 1, by default 1 if url starts with "https:")'#10
-         + '                   httponly=  htpOnly flag (0 or 1, by default 0)'#10
-         + '  -javascript      Name of a JavaScript file to execute just before taking the snapshot'#10
+         + '  -w, --width       Width of the snapshot, between 1 and 2048 (default 1024)'#10
+         + '  -h, --height      Height of the snapshot, between 1 and 2048 (default 768)'#10
+         + '                    When output format is a PDF, this parameter is ignored'#10
+         + '  -d, --delay       Delay in milliseconds, between 100 ms and 30 sec (default 1 sec)'#10
+         + '  -s, --scale       Scale of the website relative to 96dpi, between 0.1 and 10.0 (default 1.0)'#10
+         + '  --quality         Output JPEG quality (1 to 100, default 90)'#10
+         + '  --compression     Output PNG compresson level (0 to 9, default 7)'#10
          + #10
-         + '  -pdf-xxx         PDF output options outlined below'#10
-         + '       page-width      page width in microns (default 210000)'#10
-         + '       page-height     page height in microns (default 297000)'#10
-         + '       margins         sets all margins in points'#10
-         + '       margin-top      top margin in points (default 20)'#10
-         + '       margin-left     left margin in points (default 20)'#10
-         + '       margin-right    right margin in points (default 20)'#10
-         + '       margin-bottom   bottom margin in points (default 20)'#10
-         + '       landscape       portait (default, 0) or landscape (1)'#10
-         + '       backgrounds     enable backgrounds (1) or not (default, 0)'#10
+         + '  --cookie          set a cookie, format is CommaText, starting with name=value, then fields'#10
+         + '                    url=       cookie url (uses scheme + domain from url parameter by default)'#10
+         + '                    domain=    cookie domain (uses domain from url parameter by default)'#10
+         + '                    path=      cookie path ("/" by default)'#10
+         + '                    secure=    secure flag (0 or 1, by default 1 if url starts with "https:")'#10
+         + '                    httponly=  htpOnly flag (0 or 1, by default 0)'#10
+         + '  --javascript      Name of a JavaScript file to execute just before taking the snapshot'#10
+         + #10
+         + '  --pdf-xxx         PDF output options outlined below'#10
+         + '        page-width      page width in microns (default 210000)'#10
+         + '        page-height     page height in microns (default 297000)'#10
+         + '        margins         sets all margins in points'#10
+         + '        margin-top      top margin in points (default 20)'#10
+         + '        margin-left     left margin in points (default 20)'#10
+         + '        margin-right    right margin in points (default 20)'#10
+         + '        margin-bottom   bottom margin in points (default 20)'#10
+         + '        landscape       portait (default, 0) or landscape (1)'#10
+         + '        backgrounds     enable backgrounds (1) or not (default, 0)'#10
          ;
 
 // ParseCommandLineParameters
@@ -127,87 +127,23 @@ begin
    Result.PDFOptions.landscape := 0;
    Result.PDFOptions.backgrounds_enabled := 0;
 
-   var lastP := '';
-   for var i := 1 to ParamCount do begin
-      var p := ParamStr(i);
-      if p = '' then continue;
-      case p[1] of
-         '-', '/' : begin
-            lastP := LowerCase(Copy(p, 2));
-         end;
-      else
-         if (lastP = '?') or (lastP = 'h') or (lastP = 'help') then begin
-            Result.ErrorText := cHelp;
-         end else if lastP = 'url' then begin
-            if FileExists(p) then begin
-               if CustomPathIsRelative(p) then
-                  p := IncludeTrailingPathDelimiter(GetCurrentDir) + p;
-               p := 'file://' + p.Replace('\', '/');
-            end;
-            Result.URL := p;
-
-         end else if (lastP = 'width') or (lastP = 'w') then begin
-            Result.ErrorText := TryParseIntegerParameter('Width', p, Result.Width, 1, 2048);
-         end else if (lastP = 'height') or (lastP = 'h') then begin
-            Result.ErrorText := TryParseIntegerParameter('Height', p, Result.Height, 1, 2048);
-         end else if (lastP = 'scale') or (lastP = 's') then begin
-            Result.ErrorText := TryParseFloatParameter('Scale', p, Result.Scale, 0.1, 10);
-         end else if (lastP = 'delay') or (lastP = 'd') then begin
-            Result.ErrorText := TryParseIntegerParameter('Delay', p, Result.DelayMSec, 100, 30000);
-         end else if lastP = 'out' then begin
-            Result.OutputFilePath := p;
-         end else if lastP = 'quality' then begin
-            Result.ErrorText := TryParseIntegerParameter('Quality', p, Result.JPEGQuality, 1, 100);
-         end else if lastP = 'compression' then begin
-            Result.ErrorText := TryParseIntegerParameter('Quality', p, Result.JPEGQuality, 1, 100);
-         end else if lastP = 'cookie' then begin
-            var n := Length(Result.Cookies);
-            SetLength(Result.Cookies, n+1);
-            Result.Cookies[n] := p;
-         end else if lastP = 'javascript' then begin
-            if CustomPathIsRelative(p) then
-               p := IncludeTrailingPathDelimiter(GetCurrentDir) + p;
-            if not FileExists(p) then
-               Result.ErrorText := 'Javascript file not found "' + p + '"';
-            Result.JavaScript := p;
-         end else if lastP = 'pdf-page-width' then begin
-            Result.ErrorText := TryParseIntegerParameter('PDF-page-width', p, Result.PDFOptions.page_width, 10000, 10000000);
-         end else if lastP = 'pdf-page-height' then begin
-            Result.ErrorText := TryParseIntegerParameter('PDF-page-height', p, Result.PDFOptions.page_height, 10000, 10000000);
-         end else if lastP = 'pdf-margins' then begin
-            Result.ErrorText := TryParseIntegerParameter('PDF-margins', p, Result.PDFOptions.margin_top, 0, 10000);
-            Result.PDFOptions.margin_left := Result.PDFOptions.margin_top;
-            Result.PDFOptions.margin_right := Result.PDFOptions.margin_top;
-            Result.PDFOptions.margin_bottom := Result.PDFOptions.margin_top;
-         end else if lastP = 'pdf-margin-top' then begin
-            Result.ErrorText := TryParseIntegerParameter('PDF-margin-top', p, Result.PDFOptions.margin_top, 0, 10000);
-         end else if lastP = 'pdf-margin-left' then begin
-            Result.ErrorText := TryParseIntegerParameter('PDF-margin-left', p, Result.PDFOptions.margin_left, 0, 10000);
-         end else if lastP = 'pdf-margin-right' then begin
-            Result.ErrorText := TryParseIntegerParameter('PDF-margin-right', p, Result.PDFOptions.margin_right, 0, 10000);
-         end else if lastP = 'pdf-margin-bottom' then begin
-            Result.ErrorText := TryParseIntegerParameter('PDF-margin-bottom', p, Result.PDFOptions.margin_bottom, 0, 10000);
-         end else if lastP = 'pdf-landscape' then begin
-            Result.ErrorText := TryParseIntegerParameter('PDF-landscape', p, Result.PDFOptions.landscape, 0, 1);
-         end else if lastP = 'pdf-title' then begin
-            Result.PDFTitle := p;
-         end else if lastP = 'pdf-url' then begin
-            Result.PDFURL := p;
-         end else if lastP = 'pdf-backgrounds' then begin
-            Result.ErrorText := TryParseIntegerParameter('PDF-backgrounds', p, Result.PDFOptions.backgrounds_enabled, 0, 1);
-//      property scale_factor          : integer                 read Fscale_factor             write Fscale_factor          default 0;
-//      property header_footer_enabled : boolean                 read Fheader_footer_enabled    write Fheader_footer_enabled default False;
-//      property selection_only        : boolean                 read Fselection_only           write Fselection_only        default False;
-//      property landscape             : boolean                 read Flandscape                write Flandscape             default False;
-//      property backgrounds_enabled   : boolean                 read Fbackgrounds_enabled      write Fbackgrounds_enabled   default False;
-         end else begin
-            Result.ErrorText := 'Unsupported parameter "' + p + '"';
-         end;
-         lastP := '';
-      end;
-      if Result.ErrorText <> '' then Exit;
+   if ParamCount < 2 then begin
+      Result.ErrorText := cHelp;
+      Exit;
    end;
 
+   // first parameter is url_or_file
+
+   Result.URL := ParamStr(1);
+   if FileExists(Result.URL) then begin
+      if CustomPathIsRelative(Result.URL) then
+         Result.URL := IncludeTrailingPathDelimiter(GetCurrentDir) + Result.URL;
+      Result.URL := 'file://' + Result.URL.Replace('\', '/');
+   end;
+
+   // last parameter is output_file
+
+   Result.OutputFilePath := ParamStr(ParamCount);
    if CustomPathIsRelative(Result.OutputFilePath) then
       Result.OutputFilePath := IncludeTrailingPathDelimiter(GetCurrentDir) + Result.OutputFilePath;
    var ext := LowerCase(ExtractFileExt(Result.OutputFilePath));
@@ -222,6 +158,77 @@ begin
    else begin
       Result.ErrorText := 'Unsupported output file format "' + Result.OutputFilePath + '"';
       Exit;
+   end;
+
+   // parse arguments in between
+
+   var lastP := '';
+   for var i := 2 to ParamCount-1 do begin
+      var p := ParamStr(i);
+      if p = '' then continue;
+      case p[1] of
+         '-', '/' : begin
+            lastP := LowerCase(Copy(p, 2));
+         end;
+      else
+         if (lastP = '?') or (lastP = 'h') or (lastP = '-help') then begin
+            Result.ErrorText := cHelp;
+         end else if (lastP = '-width') or (lastP = 'w') then begin
+            Result.ErrorText := TryParseIntegerParameter('Width', p, Result.Width, 1, 2048);
+         end else if (lastP = '-height') or (lastP = 'h') then begin
+            Result.ErrorText := TryParseIntegerParameter('Height', p, Result.Height, 1, 2048);
+         end else if (lastP = '-scale') or (lastP = 's') then begin
+            Result.ErrorText := TryParseFloatParameter('Scale', p, Result.Scale, 0.1, 10);
+         end else if (lastP = '-delay') or (lastP = 'd') then begin
+            Result.ErrorText := TryParseIntegerParameter('Delay', p, Result.DelayMSec, 100, 30000);
+         end else if lastP = '-quality' then begin
+            Result.ErrorText := TryParseIntegerParameter('Quality', p, Result.JPEGQuality, 1, 100);
+         end else if lastP = '-compression' then begin
+            Result.ErrorText := TryParseIntegerParameter('Quality', p, Result.JPEGQuality, 1, 100);
+         end else if lastP = '-cookie' then begin
+            var n := Length(Result.Cookies);
+            SetLength(Result.Cookies, n+1);
+            Result.Cookies[n] := p;
+         end else if lastP = '-javascript' then begin
+            if CustomPathIsRelative(p) then
+               p := IncludeTrailingPathDelimiter(GetCurrentDir) + p;
+            if not FileExists(p) then
+               Result.ErrorText := 'Javascript file not found "' + p + '"';
+            Result.JavaScript := p;
+         end else if lastP = '-pdf-page-width' then begin
+            Result.ErrorText := TryParseIntegerParameter('PDF-page-width', p, Result.PDFOptions.page_width, 10000, 10000000);
+         end else if lastP = '-pdf-page-height' then begin
+            Result.ErrorText := TryParseIntegerParameter('PDF-page-height', p, Result.PDFOptions.page_height, 10000, 10000000);
+         end else if lastP = '-pdf-margins' then begin
+            Result.ErrorText := TryParseIntegerParameter('PDF-margins', p, Result.PDFOptions.margin_top, 0, 10000);
+            Result.PDFOptions.margin_left := Result.PDFOptions.margin_top;
+            Result.PDFOptions.margin_right := Result.PDFOptions.margin_top;
+            Result.PDFOptions.margin_bottom := Result.PDFOptions.margin_top;
+         end else if lastP = '-pdf-margin-top' then begin
+            Result.ErrorText := TryParseIntegerParameter('PDF-margin-top', p, Result.PDFOptions.margin_top, 0, 10000);
+         end else if lastP = '-pdf-margin-left' then begin
+            Result.ErrorText := TryParseIntegerParameter('PDF-margin-left', p, Result.PDFOptions.margin_left, 0, 10000);
+         end else if lastP = '-pdf-margin-right' then begin
+            Result.ErrorText := TryParseIntegerParameter('PDF-margin-right', p, Result.PDFOptions.margin_right, 0, 10000);
+         end else if lastP = '-pdf-margin-bottom' then begin
+            Result.ErrorText := TryParseIntegerParameter('PDF-margin-bottom', p, Result.PDFOptions.margin_bottom, 0, 10000);
+         end else if lastP = '-pdf-landscape' then begin
+            Result.ErrorText := TryParseIntegerParameter('PDF-landscape', p, Result.PDFOptions.landscape, 0, 1);
+         end else if lastP = '-pdf-title' then begin
+            Result.PDFTitle := p;      // undocumented, does not seem to work
+         end else if lastP = '-pdf-url' then begin
+            Result.PDFURL := p;        // undocumented, does not seem to work
+         end else if lastP = '-pdf-backgrounds' then begin
+            Result.ErrorText := TryParseIntegerParameter('PDF-backgrounds', p, Result.PDFOptions.backgrounds_enabled, 0, 1);
+//      property scale_factor          : integer                 read Fscale_factor             write Fscale_factor          default 0;
+//      property header_footer_enabled : boolean                 read Fheader_footer_enabled    write Fheader_footer_enabled default False;
+//      property selection_only        : boolean                 read Fselection_only           write Fselection_only        default False;
+         end else begin
+            Result.ErrorText := 'Unsupported parameter "' + p + '"';
+         end;
+         lastP := '';
+      end;
+      if Result.ErrorText <> '' then Exit;
    end;
 
    if lastP <> '' then begin
